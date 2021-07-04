424ea871b8ffa6d709f8945cb5ae9eba45334271
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2021-07-04 16:43:03 +0200

    Added a new challenge project

---
+++ Create_With_Code/Chellange2-BasicGameplay/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private Vector3 Vector3 = new Vector3(-1, 0, 0);
+
+    private DateTime lastKeyPressedDatetime = DateTime.MinValue;
+    private int pressKeyIntervalLimitInSecond = 1;
+
+    private bool CanUserPressKey => (DateTime.Now - lastKeyPressedDatetime) > TimeSpan.FromSeconds(pressKeyIntervalLimitInSecond);
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"{(DateTime.Now - lastKeyPressedDatetime)} -> {CanUserPressKey}");
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && CanUserPressKey)
+        {
+            Instantiate(dogPrefab, transform.position - Vector3, dogPrefab.transform.rotation);
+            lastKeyPressedDatetime = DateTime.Now;
+        }
+    }
+}
