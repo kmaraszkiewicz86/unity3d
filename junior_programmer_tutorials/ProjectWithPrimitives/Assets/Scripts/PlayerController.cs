149e7a26c8959229d110b6c8224000aea8c49ded
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2021-07-27 22:45:27 +0200

    Added player move functionalities to a ProjectWithPrimitives project

--- junior_programmer_tutorials/ProjectWithPrimitives/Assets/Scripts/PlayerController.cs
+++ junior_programmer_tutorials/ProjectWithPrimitives/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,65 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce = 10f;
+
+    public float speed = 10f;
+
+    public float gravityForce = 1f;
+
+    public float zBoundLimit = 9.2f;
+
+    public float xBoundLimit = 22.6f;
+
+    private bool isAtBoundLimit = false;
+
+    private bool isOnGround = true;
+
     private float horizontalMovement;
+
+    private float verticalMovement;
 
     private Rigidbody playerRigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
+        Physics.gravity *= gravityForce;
         playerRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMovement = Input.GetAxis("Horizontal");
+        PlayerMove();
+        CheckBoundLimitConstarin();
+    }
 
-        
+    private void PlayerMove()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+
+        playerRigidBody.AddForce(Vector3.forward * verticalMovement * speed);
+        playerRigidBody.AddForce(Vector3.right * horizontalMovement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !isAtBoundLimit)
+        {
+            playerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void CheckBoundLimitConstarin()
+    {
+        if (transform.position.z >= zBoundLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundLimit);
+        }
+
+        if (transform.position.z <= -zBoundLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBoundLimit);
+        }
     }
 }
